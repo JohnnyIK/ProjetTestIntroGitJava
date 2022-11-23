98cec9c4ff4146f2705fd0b4709d4366413ccf21
JohnnyIK <johnnybutin@yahoo.fr>
2022-11-23 11:11:07 +0100

    Initialisation du git

---
+++ src/fr/isika/cda22/git/testjava/LaunchTest.java
@@ -0,0 +1,10 @@
+package fr.isika.cda22.git.testjava;
+
+public class LaunchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
